035ef335b0c0114ae5f4f24b3683fbe33df5ca54
Alindah <AlindaHeng@gmail.com>
2020-12-04 22:36:51 -0800

    Added camera movements

--- Artist's 3D Reference/Assets/Scripts/CameraMovement.cs
+++ Artist's 3D Reference/Assets/Scripts/CameraMovement.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    Vector3 initialPosition = gameObject.transform.position;
+    [SerializeField] private Camera cameraUserView;
+    [SerializeField] private GameObject target;
+    [SerializeField] private float movementSpeed = 0.1f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        print("Ready for action!");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Lock view onto center of world while holding shift
+        if (Input.GetKey(KeyCode.LeftShift))
+            transform.LookAt(target.transform);
+
+        // Panning
+        if (Input.GetKey("w"))
+            transform.Translate(0, movementSpeed, 0, cameraUserView.transform);
+        else if (Input.GetKey("a"))
+            transform.Translate(-movementSpeed, 0, 0, cameraUserView.transform);
+        else if (Input.GetKey("s"))
+            transform.Translate(0, -movementSpeed, 0, cameraUserView.transform);
+        else if (Input.GetKey("d"))
+            transform.Translate(movementSpeed, 0, 0, cameraUserView.transform);
+
+        // Zooming
+        if (Input.GetKey("-"))
+            transform.Translate(0, 0, -movementSpeed, cameraUserView.transform);
+        else if (Input.GetKey("="))
+            transform.Translate(0, 0, movementSpeed, cameraUserView.transform);
+
+        // Rotating
+        if (Input.GetKey("q"))
+            transform.RotateAround(target.transform.position, Vector3.up, 200 * Time.deltaTime);
+        else if (Input.GetKey("e"))
+            transform.RotateAround(target.transform.position, Vector3.down, 200 * Time.deltaTime);        
     }
 }
